030b5a805617538a1400756067307bc42be3bd2d
Yathu Arul <yathu@outlook.com>
2023-12-02 02:26:06 +1100

    Added checkWin function to check when a player wins

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Column from "./Column";
 
 const ROWS = 6;
@@ -10,6 +10,32 @@ const Board = () => {
   const [slots, setSlots] = useState(Array(COLUMNS).fill(null).map(() => Array(ROWS).fill(null)));
   const [playerMove, setPlayerMove] = useState(FIRST_PLAYER);
 
+  useEffect(() => {
+    console.log(checkWin(slots));
+  }, [slots])
+
+  const checkWin = (slots) => {
+    for (let i = 0; i < COLUMNS; i++) {
+      for (let j = 0; j < ROWS; j++) {
+        if (slots[i][j] !== null) {
+          if (j + 3 < ROWS && slots[i][j] === slots[i][j+1] && slots[i][j] === slots[i][j+2] && slots[i][j] === slots[i][j+3]) {
+            return slots[i][j];
+          }
+          if (i + 3 < COLUMNS && slots[i][j] === slots[i+1][j] && slots[i][j] === slots[i+2][j] && slots[i][j] === slots[i+3][j]) {
+            return slots[i][j];
+          }
+          if (i + 3 < COLUMNS && j + 3 < ROWS && slots[i][j] === slots[i+1][j+1] && slots[i][j] === slots[i+2][j+2] && slots[i][j] === slots[i+3][j+3]) {
+            return slots[i][j];
+          }
+          if (i + 3 < COLUMNS && j - 3 >= 0 && slots[i][j] === slots[i+1][j-1] && slots[i][j] === slots[i+2][j-2] && slots[i][j] === slots[i+3][j-3]) {
+            return slots[i][j];
+          }
+        }
+      }
+    }
+    return null;
+  }
+
   const handleClick = (i) => {
     const newSlots = slots.map(row => [...row]);
     for (let j = ROWS-1; j >= 0; j--) {
@@ -19,7 +45,6 @@ const Board = () => {
       }
     }
     setSlots(newSlots);
-    console.log(newSlots);
     setPlayerMove(playerMove === FIRST_PLAYER ? SECOND_PLAYER : FIRST_PLAYER);
   }
 
